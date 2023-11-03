da71d6911644f5b9f70e21497b1684f0122ed26f
willofD13 <leonidastsiakas13@gmail.com>
2023-11-03 20:09:52 +0200

    Set up user-post association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :posts
+
     validates :username, presence: true, uniqueness: true, length: {maximum: 10}
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: {maximum: 8}
