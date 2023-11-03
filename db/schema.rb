69fc45a72e249b11c17b20a6bf71e858feda17d9
willofD13 <leonidastsiakas13@gmail.com>
2023-11-03 20:00:21 +0200

    Create a second Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_03_163046) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_03_175910) do
+  create_table "posts", force: :cascade do |t|
+    t.string "link"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
