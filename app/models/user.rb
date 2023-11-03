eeda10fa535567d24ccedd93322ab02207d0054e
willofD13 <leonidastsiakas13@gmail.com>
2023-11-03 20:37:47 +0200

    Establish validations and associations for comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :posts
+    has_many :comments
 
     validates :username, presence: true, uniqueness: true, length: {maximum: 10}
     validates :email, presence: true, uniqueness: true
