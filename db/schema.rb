6ae054c67d409f83cc3736397d296dcbfb6e03e9
willofD13 <leonidastsiakas13@gmail.com>
2023-11-03 20:35:14 +0200

    Create comment model and run the migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_03_175910) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_03_183433) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "link"
     t.integer "user_id"
